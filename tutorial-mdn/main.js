7057b1e7ce070291b6e707a49d5164f7037c8963
Dmitry Ishchenko <19191458+DmitriyIshchenko@users.noreply.github.com>
2024-11-02 16:13:19 +0300

    feat: create a skeleton template

--- tutorial-mdn/main.js
+++ tutorial-mdn/main.js
@@ -1,1 +1,16 @@
 import "./style.css";
+
+function draw() {
+  // retrieve the canvas node
+  const canvas = document.getElementById("tutorial");
+
+  if (canvas.getContext) {
+    // script needs to access the rendering context to draw something
+    const ctx = canvas.getContext("2d");
+  } else {
+    // unsupported canvas fallback
+  }
+}
+
+// page has to load before drawing
+window.addEventListener("load", draw);
