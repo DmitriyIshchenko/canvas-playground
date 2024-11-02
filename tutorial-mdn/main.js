4691d6ed4cfdf2e868e862c7b203d5492e95ad44
Dmitry Ishchenko <19191458+DmitriyIshchenko@users.noreply.github.com>
2024-11-02 16:25:25 +0300

    feat: create a rectrangular shapes

--- tutorial-mdn/main.js
+++ tutorial-mdn/main.js
@@ -1,5 +1,8 @@
 import "./style.css";
 
+// page has to load before drawing
+window.addEventListener("load", draw);
+
 function draw() {
   // retrieve the canvas node
   const canvas = document.getElementById("tutorial");
@@ -7,10 +10,29 @@ function draw() {
   if (canvas.getContext) {
     // script needs to access the rendering context to draw something
     const ctx = canvas.getContext("2d");
+
+    drawOverlappingRects(ctx);
+    drawRectangularShape(ctx);
   } else {
     // unsupported canvas fallback
   }
 }
 
-// page has to load before drawing
-window.addEventListener("load", draw);
+function drawOverlappingRects(ctx) {
+  ctx.fillStyle = "rgb(200 0 0)";
+  ctx.fillRect(10, 10, 50, 50);
+
+  ctx.fillStyle = "rgb(0 0 200 / 50%)";
+  ctx.fillRect(30, 30, 50, 50);
+}
+
+function drawRectangularShape(ctx) {
+  ctx.fillStyle = "rgb(0 0 0 )";
+
+  // draw a large square
+  ctx.fillRect(25, 25, 100, 100);
+  // erase the middle
+  ctx.clearRect(45, 45, 60, 60);
+  // draw a small empty square inside
+  ctx.strokeRect(50, 50, 50, 50);
+}
