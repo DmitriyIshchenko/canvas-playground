e824c6510084f09f14c73900778217c076620caa
Dmitry Ishchenko <19191458+DmitriyIshchenko@users.noreply.github.com>
2024-11-05 21:02:59 +0300

    feat: create an anymated solar system

--- tutorial-mdn/main.js
+++ tutorial-mdn/main.js
@@ -2,9 +2,13 @@ import "./style.css";
 
 import { drawShapes } from "./lessons/basic-shapes";
 import { drawStyledShapes } from "./lessons/styles-and-colors";
+import { init as drawSolarSystem } from "./lessons/basic-animations";
 
 // page has to load before drawing
-window.addEventListener("load", () => {
-  // drawShapes();
-  drawStyledShapes();
-});
+// window.addEventListener("load", () => {
+//   // drawShapes();
+//   // drawStyledShapes();
+//   drawAnimated();
+// });
+
+drawSolarSystem();
