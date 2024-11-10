64b02cdaa0b5158b8170679e33e2040649997907
Dmitry Ishchenko <19191458+DmitriyIshchenko@users.noreply.github.com>
2024-11-10 21:32:24 +0300

    feat: draw circles

--- yt-tutorial/main.js
+++ yt-tutorial/main.js
@@ -15,9 +15,22 @@ c.fillStyle = "rgba(0, 255, 0, 0.5)";
 c.fillRect(300, 300, 100, 100);
 
 // LINES
+
 c.beginPath();
 c.moveTo(50, 300);
 c.lineTo(300, 100);
 c.lineTo(400, 300);
 c.strokeStyle = "#fa34a3";
 c.stroke();
+
+// ARCS AND CIRCLES
+
+for (let i = 0; i < 100; i++) {
+  const x = Math.random() * window.innerWidth;
+  const y = Math.random() * window.innerHeight;
+
+  c.beginPath();
+  c.arc(x, y, 30, 0, Math.PI * 2, false);
+  c.strokeStyle = "blue";
+  c.stroke();
+}
