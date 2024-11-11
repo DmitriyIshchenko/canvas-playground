bc49140ad4c2a2aa4bcc31c7d88a0d68dc2d7cd7
Dmitry Ishchenko <19191458+DmitriyIshchenko@users.noreply.github.com>
2024-11-11 22:22:24 +0300

    refactor: create a circle class

--- yt-tutorial/circle-animated.js
+++ yt-tutorial/circle-animated.js
@@ -9,28 +9,55 @@ const c = canvas.getContext("2d");
 
 // CIRCLE
 
-let x = Math.random() * innerWidth;
-let y = Math.random() * innerHeight;
-let dx = (Math.random() - 0.5) * 10;
-let dy = (Math.random() - 0.5) * 10;
-const radius = 30;
+class Circle {
+  constructor(x, y, dx, dy, radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.strokeStyle = "blue";
+    c.stroke();
+  }
+
+  update() {
+    // switch direction
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0)
+      this.dx *= -1;
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0)
+      this.dy *= -1;
+
+    // update coords
+    this.x += this.dx;
+    this.y += this.dy;
+
+    this.draw();
+  }
+}
+
+const circles = [];
+for (let i = 0; i < 100; i++) {
+  const radius = 30;
+  // prevent from sticking to the edges
+  const x = Math.random() * (innerWidth - radius * 2) + radius;
+  const y = Math.random() * (innerHeight - radius * 2) + radius;
+
+  const dx = (Math.random() - 0.5) * 2;
+  const dy = (Math.random() - 0.5) * 2;
+
+  circles.push(new Circle(x, y, dx, dy, radius));
+}
 
 function animate() {
   requestAnimationFrame(animate); // creates a loop
   c.clearRect(0, 0, innerWidth, innerHeight); // clear the canvas for each frame
 
-  c.beginPath();
-  c.arc(x, y, radius, 0, Math.PI * 2, false);
-  c.strokeStyle = "blue";
-  c.stroke();
-
-  // switch direction
-  if (x + radius > innerWidth || x - radius < 0) dx *= -1;
-  if (y + radius > innerHeight || y - radius < 0) dy *= -1;
-
-  // update coords
-  x += dx;
-  y += dy;
+  circles.forEach((circle) => circle.update());
 }
 
 animate(); // init
