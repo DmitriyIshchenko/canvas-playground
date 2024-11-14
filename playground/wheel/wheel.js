fa0deaefded23caf5cfee832f85eeeb006d6d6e9
Dmitry Ishchenko <19191458+DmitriyIshchenko@users.noreply.github.com>
2024-11-14 12:08:50 +0300

    feat: create a wheel class

--- playground/wheel/wheel.js
+++ playground/wheel/wheel.js
@@ -4,12 +4,6 @@ canvas.height = 300;
 
 const ctx = canvas.getContext("2d");
 
-const WHEEL = {
-  centerX: 150,
-  centerY: 150,
-  radius: 140,
-};
-
 const LOTS = {
   lotA: 100,
   lotB: 50,
@@ -17,16 +11,69 @@ const LOTS = {
   lotD: 30,
 };
 
+class Wheel {
+  constructor() {
+    this.settings = {
+      centerX: 150,
+      centerY: 150,
+      radius: 140,
+    };
+
+    this.segments = this.createSegments(LOTS);
+    this.frame = null;
+    this.spinDuration = 5000;
+  }
+
+  createSegments(lots) {
+    let startAngle = 0;
+    const sum = Object.values(lots).reduce((acc, cur) => acc + cur, 0);
+
+    return Object.entries(lots).map(([name, value]) => {
+      const endAngle = startAngle + (2 * Math.PI * value) / sum;
+      const segment = new Segment(name, this.settings, startAngle, endAngle);
+      startAngle = endAngle;
+
+      return segment;
+    });
+  }
+
+  startAnimation() {
+    this.frame = window.requestAnimationFrame(this.startAnimation.bind(this));
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    this.segments.forEach((segment) => segment.update());
+  }
+
+  stopAnimation() {
+    if (this.frame) {
+      window.cancelAnimationFrame(this.frame);
+      this.frame = null;
+    }
+  }
+
+  spin() {
+    if (!this.frame) {
+      this.startAnimation();
+    }
+
+    setTimeout(() => {
+      this.stopAnimation();
+    }, this.spinDuration);
+  }
+}
+
 class Segment {
-  constructor(name, startAngle, endAngle) {
+  constructor(name, settings, startAngle, endAngle) {
     this.name = name;
+    this.settings = settings;
+
     this.startAngle = startAngle;
     this.endAngle = endAngle;
     this.rotation = 0;
   }
 
   draw() {
-    const { centerX, centerY, radius } = WHEEL;
+    const { centerX, centerY, radius } = this.settings;
     ctx.save();
     ctx.translate(centerX, centerY);
     ctx.rotate(this.rotation);
@@ -43,13 +90,15 @@ class Segment {
   }
 
   drawText() {
-    const { centerX, centerY } = WHEEL;
+    const { centerX, centerY } = this.settings;
 
     ctx.save();
 
     ctx.translate(centerX, centerY);
     // middle of the segment
-    ctx.rotate(this.startAngle + (this.endAngle - this.startAngle) / 2);
+    ctx.rotate(
+      this.startAngle + (this.endAngle - this.startAngle) / 2 + this.rotation
+    );
 
     ctx.font = "16px serif";
     ctx.fillStyle = "black";
@@ -61,32 +110,14 @@ class Segment {
   update() {
     this.draw();
 
-    // this.rotation += 0.01;
+    // this.rotation *= 0.99;
 
-    // this.startAngle += 0.01;
-    // this.endAngle += 0.01;
+    this.startAngle += 0.01;
+    this.endAngle += 0.01;
   }
 }
 
-function animate() {
-  window.requestAnimationFrame(animate);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+////////////////////////
 
-  segments.forEach((segment) => segment.update());
-}
-
-const segments = [];
-function generateSegments() {
-  let startAngle = 0;
-  const sum = Object.values(LOTS).reduce((acc, cur) => acc + cur, 0);
-
-  Object.entries(LOTS).forEach(([name, value]) => {
-    const endAngle = startAngle + (2 * Math.PI * value) / sum;
-    segments.push(new Segment(name, startAngle, endAngle));
-    startAngle = endAngle;
-  });
-}
-
-generateSegments();
-
-animate();
+const wheel = new Wheel();
+wheel.spin();
